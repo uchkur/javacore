75167e3005cf9ab485870c4d98646d5b20d9400e
Исмаилов Роман Рауфович <SBT-Ismailov-RR@mail.ca.sbrf.ru>
2018-04-11 16:02:47 +0300

    init

---
+++ lesson1/src/test/java/Task1Test.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task1Test {
+    @org.junit.jupiter.api.Test
+    void main() {
+        System.out.println("Test main:");
+        Task1.main( new String[] {
+                "aaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbb"
+                , "xxxxxx"
+                , "yyyyyyyyy"
+        });
+    }
+
+}
